5cb2bb4e324e00e7099caceeca5e8621a368f2cb
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-08-22 23:20:24 +0300

    Adds Conditionals examples

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Hello!");
+
+// Conditionals and Loops
+
+/** 1. Conditionals
+ Structurile conditionale sunt utilizate pentru a executa un bloc de cod in functie de 
+ evaluarea unei expresii sau a unei conditii. 
+ Rezultatul evaluarii conditiei va avea intotdeauna 
+ tipul de date boolean (true sau false).
+ */
+
+ /**if - Este cea  mai simpla structura conditionala. 
+ Blocul de cod asociat cu ‘if’ este executat doar daca expresia/conditia este adevarata.*/
+
+ let isChoreDone = false;
+ let hasEaten = false;
+
+ if (isChoreDone) {
+    console.log("Te poti uita la desene animate");
+ }
+
+ if (!isChoreDone || !hasEaten) {
+
+    if(!isChoreDone) {
+        console.log("Inca mai ai de lucru! Te poti uita la desene animate dupa ce ai terminat treaba.");
+    }
+
+    if (!hasEaten) {
+        console.log("Inca nu ai terminat de mancat.");
+    }
+    
+ }
+
+ 
